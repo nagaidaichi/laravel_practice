8ec78bcb72bd34a2053cf13b942107fa5ed0399e
nagai <admblgld@gmail.com>
2020-06-18 13:30:43 +0900

    second commit

---
+++ samazon/resources/views/products/edit.blade.php
@@ -0,0 +1,21 @@
+<h1>Updae Products</h1>
+
+<form method="POST" action="/products/{{ $product->id}}">
+    {{ csrf_field() }}
+    <input type="hidden" name="_method" value="PUT">
+    <input type="text" name="name" value="{{ $product->name }}">
+    <textarea name='description' >{{ $product->description }}</textarea>
+    <input type='number' name='price' value='{{ $product->price}}'>
+    <select name='category_id'>
+    @foreach ($categories as $category)
+        @if ($category->id == $product->category_id)
+            <option value='{{ $category_id }}' selected>{{ $category->name}}</option>
+        @else
+            <option value='{{$category->id}}'>{{ $category_name}}</option>
+        @endif
+    @endforeach
+    </select>
+    <button type='submit'>Update</button>
+</form>
+
+<a href='/products'>Back</a>
